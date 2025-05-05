4fe9c4490f76e39fcc4b395b47fb0819e084d94f
Alex <alexandermaiso99@gmail.com>
2025-05-05 19:12:49 +0800

    Update something

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -48,7 +48,7 @@ const Hero = () => {
           <h1 className={`${styles.heroHeadText} text-white`}>
             Hi, I'm{" "}
             <span className="animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent font-black">
-              Alex <span className="hidden sm:inline"></span>
+              Alexa <span className="hidden sm:inline"></span>
             </span>
           </h1>
 
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/tae/', // 👈 Add this line
   plugins: [react()],
 })
